4859c2ba4671dc7275a8a7c83b304005ee010d45
Steve Carmel <steve2christ@users.noreply.github.com>
2021-05-12 00:20:46 +0200

    #4 implementation_of_the_home_page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { rootingPath } from './shared/rooting-path'
+import { HomepageComponent } from './homepage/homepage.component'
+import { SearchResultsComponent } from './search-results/search-results.component'
+import { AboutUsComponent } from './about-us/about-us.component'
+import { ImpressumComponent } from './impressum/impressum.component'
+import { PortofolioComponent } from './portofolio/portofolio.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/' + rootingPath.home},
+  {path: rootingPath.home, component: HomepageComponent},
+  {path: rootingPath.search_results + '/:pillId', component: SearchResultsComponent},
+  {path: rootingPath.about, component: AboutUsComponent},
+  {path: rootingPath.about + '/:pillId', component: PortofolioComponent},
+  {path: rootingPath.impressum, component: ImpressumComponent},
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/shared/rooting-path.ts
@@ -0,0 +1,6 @@
+export const rootingPath = {
+  home: 'home',
+  search_results: 'search_results',
+  about: 'about_us',
+  impressum: 'impressum'
+}
