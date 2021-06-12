03776b9e8b5582e0764a0aa05268d7378c42fe2e
Bertin19 <juniorwagueu@gmail.com>
2021-06-12 18:18:04 +0200

    Suchergebnis set, image and statistics

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild } from "@angular/core";
+import { ElementRef } from "@angular/core";
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+import { rootingPath } from "../shared/rooting-path"
 
 @Component({
   selector: 'app-search-results',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent implements OnInit {
+  mapWidth: number = 300
+  onMapClick: boolean = false
+  readonly homePath: string
+  bundesLandName: string = ""
 
-  constructor() { }
+  constructor(
+    private activeRoute: ActivatedRoute
+    ) { 
+      this.homePath = '/' + rootingPath.home
+    }
 
   ngOnInit(): void {
+    this.activeRoute.params.subscribe(
+      (params: any) => this.bundesLandName = params["pillId"]
+    )
+    console.log(this.bundesLandName)
+  }
+
+  
+  changeImageSize(): void {
+    this.onMapClick ? this.mapWidth = 100 : this.mapWidth = 400
+    //document.getElementById("bundesLandMap").style.width=width
+    this.onMapClick = !this.onMapClick
   }
 
 }
