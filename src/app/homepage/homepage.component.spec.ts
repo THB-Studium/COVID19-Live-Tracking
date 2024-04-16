b12b5ba117470404d3a3babdf80cdb57d771a99e
Steve Carmel <egoue2001@gmail.com>
2024-04-17 01:23:23 +0200

    ng upgrade from vers. 14 to 15 done

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { HomepageComponent } from './homepage.component'
 import {HttpClientTestingModule} from '@angular/common/http/testing'
 import {RouterTestingModule} from '@angular/router/testing'
-import {MatAutocompleteModule} from '@angular/material/autocomplete'
+import {MatLegacyAutocompleteModule as MatAutocompleteModule} from '@angular/material/legacy-autocomplete'
 
 describe('HomepageComponent', () => {
   let component: HomepageComponent
